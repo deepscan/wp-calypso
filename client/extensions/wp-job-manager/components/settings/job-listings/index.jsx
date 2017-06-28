3191ea7f06904dc34ce40e41b9a2be323ac2f3bd
Donna Peplinskie <donnapep@gmail.com>
2017-06-28 11:17:31 -0400

    Add <Field/> components to connect each input to the store
    http://redux-form.com/6.8.0/docs/GettingStarted.md/#step-3-of-4-form-fie
    ld-components
    
    The ‘component’ attribute of <Field/> takes a function that is
    responsible for rendering the form element -
    http://redux-form.com/6.8.0/docs/api/Field.md/#2-a-stateless-function
    
    It uses the input props to connect the component to Redux state -
    http://redux-form.com/6.8.0/docs/api/Field.md/#input-props
    
    The parent <Settings/> component takes care of rendering the form elements.

--- client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
+++ client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
@@ -2,7 +2,8 @@
  * External dependencies
  */
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { Field, formValueSelector, reduxForm } from 'redux-form';
 import { localize } from 'i18n-calypso';
 import { flowRight } from 'lodash';
 
@@ -14,49 +15,19 @@ import Card from 'components/card';
 import ExternalLink from 'components/external-link';
 import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
-import FormRadio from 'components/forms/form-radio';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
-import FormTextInput from 'components/forms/form-text-input';
-import FormToggle from 'components/forms/form-toggle/compact';
-import ListingsPerPage from './listings-per-page';
 import SectionHeader from 'components/section-header';
 
 class JobListings extends Component {
-	state = {
-		job_manager_category_filter_type: 'any',
-		job_manager_date_format: 'relative',
-		job_manager_enable_categories: false,
-		job_manager_enable_default_category_multiselect: false,
-		job_manager_enable_types: true,
-		job_manager_google_maps_api_key: '',
-		job_manager_hide_expired: true,
-		job_manager_hide_expired_content: true,
-		job_manager_hide_filled_positions: false,
-		job_manager_multi_job_type: false,
-		job_manager_per_page: 10,
-	};
-
-	updateRadio = name => event => this.setState( { [ name ]: event.target.value } );
-
-	updateTextInput = name => event => this.setState( { [ name ]: event.target.value } );
-
-	updateToggle = name => () => this.setState( { [ name ]: ! this.state[ name ] } );
-
 	render() {
-		const { translate } = this.props;
 		const {
-			job_manager_category_filter_type,
-			job_manager_date_format,
-			job_manager_enable_categories,
-			job_manager_enable_default_category_multiselect,
-			job_manager_enable_types,
-			job_manager_google_maps_api_key,
-			job_manager_hide_expired,
-			job_manager_hide_expired_content,
-			job_manager_hide_filled_positions,
-			job_manager_multi_job_type,
-			job_manager_per_page,
-		} = this.state;
+			perPage,
+			renderNumberInput,
+			renderRadio,
+			renderTextInput,
+			renderToggle,
+			translate,
+		} = this.props;
 
 		return (
 			<div>
@@ -72,38 +43,35 @@ class JobListings extends Component {
 								'Display {{listings /}} job listing per page',
 								'Display {{listings /}} job listings per page',
 								{
-									count: job_manager_per_page,
+									count: perPage,
 									components: {
-										listings: <ListingsPerPage onChange={ this.updateTextInput } value={ job_manager_per_page } />
+										listings:
+											<Field
+												component={ renderNumberInput }
+												name="job_manager_per_page" />
 									}
 								}
 							) }
 						</p>
 
 						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_hide_filled_positions }
-								onChange={ this.updateToggle( 'job_manager_hide_filled_positions' ) }>
-								{ translate( 'Hide filled positions' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Hide filled positions' ) ) }
+								name="job_manager_hide_filled_positions" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'Filled positions will not display in your archives.' ) }
 							</FormSettingExplanation>
 
-							<FormToggle
-								checked={ job_manager_hide_expired }
-								onChange={ this.updateToggle( 'job_manager_hide_expired' ) }>
-								{ translate( 'Hide expired listings in job archives/search' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Hide expired listings in job archives/search' ) ) }
+								name="job_manager_hide_expired" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'Expired job listings will not be searchable.' ) }
 							</FormSettingExplanation>
 
-							<FormToggle
-								checked={ job_manager_hide_expired_content }
-								onChange={ this.updateToggle( 'job_manager_hide_expired_content' ) }>
-								{ translate( 'Hide content in expired single job listings' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Hide content in expired single job listings' ) ) }
+								name="job_manager_hide_expired_content" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'Your site will display the titles of expired listings, but not the ' +
 									'content of the listings. Otherwise, expired listings display their full content ' +
@@ -121,21 +89,17 @@ class JobListings extends Component {
 				<Card>
 					<form>
 						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_enable_categories }
-								onChange={ this.updateToggle( 'job_manager_enable_categories' ) }>
-								{ translate( 'Enable listing categories' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Enable listing categories' ) ) }
+								name="job_manager_enable_categories" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'This lets users select from a list of categories when submitting a ' +
 									'job. Note!: an admin has to create categories before site users can select them.' ) }
 							</FormSettingExplanation>
 
-							<FormToggle
-								checked={ job_manager_enable_default_category_multiselect }
-								onChange={ this.updateToggle( 'job_manager_enable_default_category_multiselect' ) }>
-								{ translate( 'Default to category multiselect' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Default to category multiselect' ) ) }
+								name="job_manager_enable_default_category_multiselect" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'The category selection box will default to allowing multiple ' +
 									'selections on the [jobs] shortcode. Without this, users will only be able to ' +
@@ -151,22 +115,18 @@ class JobListings extends Component {
 								{ translate( 'Determines the logic used to display jobs when selecting multiple categories.' ) }
 							</FormSettingExplanation>
 							<FormLabel>
-								<FormRadio
-									checked={ 'any' === job_manager_category_filter_type }
-									name="job_manager_category_filter_type"
-									onChange={ this.updateRadio( 'job_manager_category_filter_type' ) }
-									value="any" />
+								<Field
+									component={ renderRadio( 'any' ) }
+									name="job_manager_category_filter_type" />
 								<span>
 									{ translate( 'Jobs will be shown if within ANY selected category' ) }
 								</span>
 							</FormLabel>
 
 							<FormLabel>
-								<FormRadio
-									checked={ 'all' === job_manager_category_filter_type }
-									name="job_manager_category_filter_type"
-									onChange={ this.updateRadio( 'job_manager_category_filter_type' ) }
-									value="all" />
+								<Field
+									component={ renderRadio( 'all' ) }
+									name="job_manager_category_filter_type" />
 								<span>
 									{ translate( 'Jobs will be shown if within ALL selected categories' ) }
 								</span>
@@ -179,21 +139,17 @@ class JobListings extends Component {
 				<Card>
 					<form>
 						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_enable_types }
-								onChange={ this.updateToggle( 'job_manager_enable_types' ) }>
-								{ translate( 'Enable listing types' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Enable listing types' ) ) }
+								name="job_manager_enable_types" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'This lets users select from a list of types when submitting a job. ' +
 									'Note!: an admin has to create types before site users can select them.' ) }
 							</FormSettingExplanation>
 
-							<FormToggle
-								checked={ job_manager_multi_job_type }
-								onChange={ this.updateToggle( 'job_manager_multi_job_type' ) }>
-								{ translate( 'Allow multiple types for listings' ) }
-							</FormToggle>
+							<Field
+								component={ renderToggle( translate( 'Allow multiple types for listings' ) ) }
+								name="job_manager_multi_job_type" />
 							<FormSettingExplanation isIndented>
 								{ translate( 'This allows users to select more than one type when submitting a job. ' +
 									'The metabox on the post editor and the selection box on the front-end job ' +
@@ -215,22 +171,18 @@ class JobListings extends Component {
 								{ translate( 'Choose how you want the published date for jobs to be displayed on the front-end.' ) }
 							</FormSettingExplanation>
 							<FormLabel>
-								<FormRadio
-									checked={ 'relative' === job_manager_date_format }
-									name="job_manager_date_format"
-									onChange={ this.updateRadio( 'job_manager_date_format' ) }
-									value="relative" />
+								<Field
+									component={ renderRadio( 'relative' ) }
+									name="job_manager_date_format" />
 								<span>
 									{ translate( 'Relative to the current date (e.g., 1 day, 1 week, 1 month ago)' ) }
 								</span>
 							</FormLabel>
 
 							<FormLabel>
-								<FormRadio
-									checked={ 'default' === job_manager_date_format }
-									name="job_manager_date_format"
-									onChange={ this.updateRadio( 'job_manager_date_format' ) }
-									value="default" />
+								<Field
+									component={ renderRadio( 'default' ) }
+									name="job_manager_date_format" />
 								<span>
 									{ translate( 'Default date format as defined in Settings' ) }
 								</span>
@@ -247,9 +199,9 @@ class JobListings extends Component {
 				<Card>
 					<form>
 						<FormFieldset>
-							<FormTextInput
-								onChange={ this.updateTextInput( 'job_manager_google_maps_api_key' ) }
-								value={ job_manager_google_maps_api_key } />
+							<Field
+								component={ renderTextInput }
+								name="job_manager_google_maps_api_key" />
 							<FormSettingExplanation>
 								{ translate(
 									'Google requires an API key to retrieve location information for job listings. ' +
@@ -275,12 +227,23 @@ class JobListings extends Component {
 	}
 }
 
+const connectComponent = connect(
+	( state ) => {
+		const selector = formValueSelector( 'listings', () => state.extensions.wpJobManager.form );
+
+		return {
+			perPage: selector( state, 'job_manager_per_page' ),
+		};
+	}
+);
+
 const createReduxForm = reduxForm( {
 	form: 'listings',
 	getFormState: state => state.extensions.wpJobManager.form,
 } );
 
 export default flowRight(
+	connectComponent,
 	localize,
 	createReduxForm,
 )( JobListings );
